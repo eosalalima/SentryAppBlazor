cd5a333bfeeca6218e571b1b762dccb5ff85d6e0
eric_salalima <eosalalima@gmail.com>
2026-08-23 13:08:06 +0800

    Updates

--- SentryAppBlazor/SentryAppBlazor/obj/Release/net10.0/SentryAppBlazor.AssemblyInfo.cs
+++ SentryAppBlazor/SentryAppBlazor/obj/Release/net10.0/SentryAppBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SentryAppBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77b4ff91bd063f67a22e804f167a1869bd3441b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e459389ff2128e3cccd6290c6170c94701785027")]
 [assembly: System.Reflection.AssemblyProductAttribute("SentryAppBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SentryAppBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
